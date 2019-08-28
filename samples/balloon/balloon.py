64986e33c3030326e7e5f63540336bad05ee6ad4
gangrim <gis20100101@outlook.com>
2019-08-28 12:15:26 +0900

    add

--- samples/balloon/balloon.py
+++ samples/balloon/balloon.py
@@ -127,8 +127,7 @@ class BalloonDataset(utils.Dataset):
             # the outline of each object instance. These are stores in the
             # shape_attributes (see json format above)
             # The if condition is needed to support VIA versions 1.x and 2.x.
-            annots=[a for a in annotations if a['regions']]
-            # a['regions']
+            annots=a['regions']
 
             # load_mask() needs the image size to convert polygons to masks.
             # Unfortunately, VIA doesn't include it in JSON, so we must read
